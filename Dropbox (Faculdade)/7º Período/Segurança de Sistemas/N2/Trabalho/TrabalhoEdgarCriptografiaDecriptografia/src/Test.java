8bd1ac951fd31a1bcf0d07cbb9166b5cfffc72b8
WALLYSON TOBIAS <wallyson.thbest@gmail.com>
2017-04-26 22:06:44 -0300

    INCLUSÃO

---
+++ Dropbox (Faculdade)/7º Período/Segurança de Sistemas/N2/Trabalho/TrabalhoEdgarCriptografiaDecriptografia/src/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ~WallysonTobias
+ */
+public class Test {
+    
+    public static void main(String args[]){
+        
+        int a = 7;
+        int b = 2;
+        int soma;
+        
+        soma = a+b;
+        
+        System.out.println(soma);
+    }
+    
+}
